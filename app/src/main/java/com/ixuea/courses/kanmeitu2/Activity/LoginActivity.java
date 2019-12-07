6f46897f103a8a573b436b2e58c6db8f0a38f120
kanmeitu2 <118493020@QQ.COM>
2019-12-07 18:10:23 +0800

    修改

---
+++ app/src/main/java/com/ixuea/courses/kanmeitu2/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ixuea.courses.kanmeitu2.Activity;
+
+import android.os.Bundle;
+
+import com.ixuea.courses.kanmeitu2.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
