c3f1cb530c4bd632daff43789baa9b29100d21bf
kanmeitu2 <118493020@QQ.COM>
2019-12-23 19:43:34 +0800

    默认更改列表

--- app/src/main/java/com/ixuea/courses/kanmeitu2/MainActivity.java
+++ app/src/main/java/com/ixuea/courses/kanmeitu2/MainActivity.java
@@ -1,14 +1,40 @@
 package com.ixuea.courses.kanmeitu2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ixuea.courses.kanmeitu2.Activity.LoginActivity;
+import com.ixuea.courses.kanmeitu2.util.SharedPreferencesUtil;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferencesUtil sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = SharedPreferencesUtil.getInstance(getApplicationContext());
+    }
+
+
+    /**
+     * 退出
+     *
+     * @param view
+     */
+    public void onLogoutClick(View view) {
+        sp.setLogin(false);
+
+        //退出后，跳转到登录界面
+        //当然大家可以根据业务逻辑调整
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
